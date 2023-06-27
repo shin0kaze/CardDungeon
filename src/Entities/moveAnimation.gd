cde839bd1bed81833234043ce3daaddad1b9579e
usr <kirill.moskva@mail.ru>
2023-06-27 21:57:30 +0600

    доделан компонент способ определить если root

--- src/Entities/moveAnimation.gd
+++ src/Entities/moveAnimation.gd
@@ -2,13 +2,24 @@
 extends Node
 class_name MoveAnimation
 
-var tween: Tween = Tween.new().bind_node(self)
+@onready var tween: Tween = create_tween()
 @onready var parent: Node2D = $"../"
 
-
 func _ready():
+	tween.bind_node(self)
 	tween.set_pause_mode(Tween.TWEEN_PAUSE_STOP)
 	tween.set_process_mode(Tween.TWEEN_PROCESS_IDLE)
 
+## Добавляет анимацию последовательно
 func move(pos: Vector2, dur: float = 0.4, easing: int = Tween.EASE_IN_OUT, trans: int = Tween.TRANS_LINEAR):
+	tween.set_parallel(false)
 	tween.tween_property(parent, "position", pos, dur).set_ease(easing).set_trans(trans)
+	return self
+
+## Добавляет анимацию параллельно
+func movp(pos: Vector2, dur: float = 0.4, easing: int = Tween.EASE_IN_OUT, trans: int = Tween.TRANS_LINEAR):
+	tween.set_parallel(false)
+	tween.tween_property(parent, "position", pos, dur).set_ease(easing).set_trans(trans)
+	return self
+
+
