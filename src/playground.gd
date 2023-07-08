f6d9eb14c903397a802bc1d23ad0ef975820b243
usr <kirill.moskva@mail.ru>
2023-07-08 15:44:43 +0600

    coderunner -> playground

--- src/playground.gd
+++ src/playground.gd
@@ -1,13 +1,15 @@
 ## Нода для быстрого запуска и проверки кода
 extends Node2D
+class_name CodeRunner
 
 func _ready():
 	C.dm("Code runned")
+	run_code()
 
 func run_code():
-	@warning_ignore("unassigned_variable")
-	var _pass
+	pass
+# === Кидать код сюда ===
 
 
 
-
+# === Кидать код выше ===
